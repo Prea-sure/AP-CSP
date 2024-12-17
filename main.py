67ff04b6a811e1a17082e7d846deecbfbcf9b13b
Prea <113808901+Prea-sure@users.noreply.github.com>
2024-12-17 13:46:50 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,21 +6,38 @@ from time import *
 def wait(sec: float):
     sleep(sec)
 
+def goodbye():
+    text.clear()
+    text.write("Goodbye! Thanks for playing", font=font)
+
 
 robot_idle = "robot-idle.gif"
 robot_run = "robot-run.gif"
+correct_mark = "correct.gif"
+incorrect_mark = "incorrect.gif"
 
 font = ("Futura", 13, "bold")
+a = rand.randint(1, 20)
+b = rand.randint(1, 20)
+
 
 wn = trtl.Screen()
 text = trtl.Turtle()
 robot = trtl.Turtle()
+correct = trtl.Turtle(); correct.penup()
+incorrect = trtl.Turtle(); incorrect.penup()
 wn.addshape(robot_idle)
 wn.addshape(robot_run)
+wn.addshape(correct_mark)
+wn.addshape(incorrect_mark)
 wn.title("Test Your Math")
 
 
-robot.resizemode("auto")
+correct.hideturtle(); correct.goto(correct.xcor() - 270, correct.ycor())
+incorrect.hideturtle(); incorrect.goto(incorrect.xcor() + 270, incorrect.ycor() - 40)
+
+correct.shape(correct_mark)
+incorrect.shape(incorrect_mark)
 robot.shape(robot_idle)
 
 robot.hideturtle()
@@ -33,7 +50,6 @@ text.hideturtle()
 text.penup()
 text.goto(text.xcor() - 220, text.ycor() + 200)
 text.write("Hello user! What is your name?", font=font)
-
 
 question: float
 user = wn.textinput("Username", "User:")
@@ -48,15 +64,38 @@ if user:
         text.clear()
         text.write("Alright! Let's begin!", font=font)
         text.clear()
-        question = wn.numinput("Question", "What is " + str(rand.randint(1, 20)) + " + " + str(rand.randint(1, 20)) + "?")
-        while 
+        question = wn.numinput("Question", "What is " + str(a) + " + " + str(b) + "?")
+        while True:
+            if a + b != question:
+                text.write("That's incorrect! " + str(a) + " + " + str(b) + " is " + str(a + b) + ". " + "Try again!", font=font)
+                incorrect.showturtle()
+                wait(2)
+                incorrect.hideturtle()
+                a = rand.randint(1, 20)
+                b = rand.randint(1, 20)
+                question = wn.numinput("Question", "What is " + str(a) + " + " + str(b) + "?")
+                text.clear()
+            else:
+                text.write("That's correct! Good job.", font=font)
+                correct.showturtle()
+                wait(2)
+                correct.hideturtle()
+                a = rand.randint(1, 20)
+                b = rand.randint(1, 20)
+                question = wn.numinput("Question", "What is " + str(a) + " + " + str(b) + "?")
+                text.clear()
+
     else:
         text.clear()
         text.write("Oh. Well, goodbye!", font=font)
         wait(1.5)
         wn.bye()
+else:
+    text.clear()
+    text.write("You must have a name!", font=font)
+    wait(1)
+    wn.bye()
 
 
-
-
+wn.onkeypress(goodbye, "y")
 wn.mainloop()
